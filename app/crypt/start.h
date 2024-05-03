7136d83af510dc1db0577899f1ee7f3de70dcf22
Dpdl <148251710+Dpdl-io@users.noreply.github.com>
2024-05-03 18:51:03 +0200

    Update start.h
    fixes, code improvement, cleanup

--- app/crypt/start.h
+++ app/crypt/start.h
@@ -1,17 +1,20 @@
-# DpdlSimpleCryptApp - a simple sample application written with Dpdl
+# DpdlSimpleCryptApp - a simple sample application module written with Dpdl
 #
 # (c)opyright 2023
 # developed by SEE Solutions
 #
 # File: app/crypt/start.h
 #
-# Example: Dpdl startup script for dpdl sample app 'DpdlSimpleCryptApp' that implements encryption and decryption of strings
-#			using the tinycrypt C library
+# Example: Dpdl startup script for dpdl sample app 'DpdlSimpleCryptApp' that implements AES-128 encryption and decryption of strings of data
+#			via Dpdl embedded C code that makes use of the tinycrypt C library
 #
 # Author: A.Costa
 # e-mail: ac@dpdl.io
 #
 #
+
+println("starting 'DpdlSimpleCryptApp' ...")
+println("")
 
 string KEY = "0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c"
 
@@ -20,13 +23,13 @@ string IV = "0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0
 dpdl_stack_var_glob_put("crypt_key", KEY)
 dpdl_stack_var_glob_put("iv", IV)
 
-println("testing encryption/decryption of strings of data using the 'tinycrypt' C library...")
+println("testing encryption/decryption of strings of data with AES-128 using the 'tinycrypt' C library...")
 
 println("")
 
 object simple_crypt = loadCode("app/crypt/dpdlSimpleCryptApp.h")
 
-string msg = "This is my message to encrypt:)"
+string msg = "This is my message I want to encrypt:)"
 
 println("message to encrypt: " + msg)
 
@@ -45,5 +48,4 @@ println("decrypting...")
 string dec_msg = simple_crypt.decrypt("dpdlSimpleCryptApp", enc_msg)
 
 println("decrypted message: " + dec_msg)
-
 
