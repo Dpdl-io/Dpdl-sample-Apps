e4fd18a2f3ed62eddb6a64347de4d325675a0dc2
Dpdl <148251710+Dpdl-io@users.noreply.github.com>
2026-03-16 10:34:58 +0100

    Update dpdlMqttUI.h

--- app/dpdlmqttapp/dpdlMqttUI.h
+++ app/dpdlmqttapp/dpdlMqttUI.h
@@ -29,7 +29,7 @@ func addMsgToList(string msg_sent)
 	int last_item = data_model_list.getSize()-1
 	msg_list.ensureIndexIsVisible(last_item)
 
-	object date = loadObj("Date")
+	object date = new("Date")
 	date_str = date.toString()
 	date_time.setText(date_str)
 end
@@ -50,44 +50,44 @@ end
 int width = 550
 int height = 500
 
-object frame = loadObj("JFrame", "DpdlMqttApp - www.dpdl.io")
-object option_frame_class = getClass("JOptionPane")
+object frame = new("JFrame", "DpdlMqttApp - www.dpdl.io")
+object option_frame_class = getObj("JOptionPane")
 
-object status_label = loadObj("JLabel", "Status: ")
+object status_label = new("JLabel", "Status: ")
 
-object data_model_list = loadObj("DefaultListModel")
-object msg_list = loadObj("JList", data_model_list)
-object msg_list_scroll = loadObj("JScrollPane")
+object data_model_list = new("DefaultListModel")
+object msg_list = new("JList", data_model_list)
+object msg_list_scroll =  new("JScrollPane")
 msg_list_scroll.setViewportView(msg_list)
 
-object border_layout_class = getClass("BorderLayout")
+object border_layout_class = getObj("BorderLayout")
 
-object layout_list1 = loadObj("GridLayout")
-object panel_list = loadObj("JPanel", layout_list1)
+object layout_list1 = new("GridLayout")
+object panel_list = new("JPanel", layout_list1)
 panel_list.add(status_label)
 panel_list.add(msg_list_scroll)
 frame.add(panel_list, border_layout_class.NORTH)
 
-object border_factory_class = getClass("BorderFactory")
-object border_layout = loadObj("BorderLayout")
-object panel_text = loadObj("JPanel", border_layout)
+object border_factory_class = getObj("BorderFactory")
+object border_layout = new("BorderLayout")
+object panel_text = new("JPanel", border_layout)
 
 
-object msg_area = loadObj("JTextArea", 220, 100)
-object scroll_pane_class = getClass("JScrollPane")
-object scroll_pane = loadObj("JScrollPane", msg_area)
+object msg_area = new("JTextArea", 220, 100)
+object scroll_pane_class = getObj("JScrollPane")
+object scroll_pane = new("JScrollPane", msg_area)
 panel_text.setBorder(border_factory_class.createTitledBorder("Message:"))
 panel_text.add(scroll_pane)
 frame.add(panel_text, border_layout_class.CENTER)
 
 
-object panel_form = loadObj("JPanel")
+object panel_form = new("JPanel")
 
-object button = loadObj("JButton")
-object date_time = loadObj("JLabel", "")
-object topic_label = loadObj("JLabel", "")
+object button = new("JButton")
+object date_time = new("JLabel", "")
+object topic_label = new("JLabel", "")
 
-object event_listener = loadObj("ActionListenerInterface")
+object event_listener = new("ActionListenerInterface")
 
 button.setLabel("Send")
 button.setActionCommand("Send_cmd");
@@ -99,7 +99,7 @@ panel_form.add(topic_label)
 
 frame.add(panel_form, border_layout_class.SOUTH)
 
-object frame_class = getClass("JFrame")
+object frame_class = getObj("JFrame")
 frame.setSize(width, height)
 frame.setDefaultCloseOperation(frame_class.EXIT_ON_CLOSE);
 frame.setVisible(true)
@@ -107,4 +107,4 @@ frame.setVisible(true)
 data_model_list.addElement("Message list->")
 
 string MSG = ""
-object MSG_STACK = loadObj("Stack")
+object MSG_STACK = new("Stack")
